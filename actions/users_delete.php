5460bdb4ae3c43e3afabb9b0727bb84cfac07f03
root <root@LucasSilva>
2023-07-20 15:16:51 -0300

    Script para deletar o registro do usuário selecionado

---
+++ actions/users_delete.php
@@ -0,0 +1,21 @@
+<?php
+if(!empty($_GET['id'])) {
+
+  include_once('../connection.php');
+
+  $id = $_GET['id'];
+
+  $sqlSelect = "SELECT * FROM usuarios WHERE id=$id";
+
+  $result = $conn->query($sqlSelect);
+
+  if($result->num_rows > 0) {
+    $sqlDelete = "DELETE FROM usuarios WHERE id='$id'";
+    
+    $result = $conn->query($sqlDelete);
+
+  }
+}
+
+header("Location: ../views/users.php")
+?>
