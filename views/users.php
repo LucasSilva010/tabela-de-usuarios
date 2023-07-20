7e1604b74b42b71da1142ed905cb0a223a22ca5d
root <root@LucasSilva>
2023-07-20 13:46:50 -0300

    Documento HTML para a exibição da tabela de usuários e eexibição de outras funções como adicionar, editar ou remover usuário.

---
+++ views/users.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tabela de usuários</title>
+
+  <!-- Boostrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+<?php include '../connection.php' ?>
+  <div class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+         <table class="table table-bordered table-striped table-hover">
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Função</th>
+            <th></th>
+          </tr>
+          <?php
+          // Consulta SQL para selecionar os dados
+          $sql = "SELECT * FROM usuarios";
+          $result = $conn->query($sql);
+          ?>   
+          <?php foreach($result as $usuario): ?>
+            <tr>
+              <td><?php echo $usuario['nome'] ?></td>
+              <td><?php echo $usuario['email'] ?></td>
+              <td><?php echo $usuario['funcao'] ?></td>
+              <td>
+                <a class="btn btn-primary btn-sm" href="./users_update.php?id=<?php echo $usuario['id']?>">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                </svg>
+                </a>
+              </td>
+            </tr>
+          <?php endforeach; ?>
+         </table>
+          <a class="btn btn-dark" href="./users_createForm.php">Adicionar um novo usuário</a>
+      </div>
+    </div>
+  </div>
+
+
+<!-- Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<!-- php -S localhost:8000 -->
+</body>
+</html>
