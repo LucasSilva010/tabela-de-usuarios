d900c24897b14dd26247a09e879eb473d4b4a78f
root <root@LucasSilva>
2023-07-20 13:44:15 -0300

    Script para conexão com o servidor MySQL

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "abrobadepatins";
+$dbname = "dados_desafio_final";
+ 
+// Connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
