c997adbceb74a9c3462f82f8889ead8ef8e8ab47
root <root@LucasSilva>
2023-07-22 11:45:09 -0300

    Alterações para melhor interação entre o documento e a validação do formulário, para proporcionar um feedback melhor para o usuário

--- views/users_createForm.php
+++ views/users_createForm.php
@@ -17,24 +17,30 @@
   <div class="row d-flex align-items-center justify-content-center">
     <div class="col-md-4">
       <!-- Formulário para registrar usuários -->
-      <form class="col-md-12" action="../actions/users_insert.php" method="post">
+      <form id="updateForm" class="col-md-12" action="../actions/users_insert.php" method="post">
         <div class="form-group">
           <label for="nameInput">Nome do usuário:</label>
-          <input type="text" class="form-control" id="nameInput" name="nameInput" placeholder="Seu nome">
+          <input type="text" class="form-control" id="nameInput" name="nameInput" placeholder="Seu nome" required>
+          <div class="invalid-feedback">Informe um nome válido (somente letras e espaços).</div>
         </div>
         <div class="form-group">
           <label for="emailInput">E-mail:</label>
-          <input type="email" class="form-control" id="emailInput" name="emailInput" placeholder="Seu email">
+          <input type="text" class="form-control" id="emailInput" name="emailInput" placeholder="Seu email" required>
+          <div class="invalid-feedback">Informe um email válido.</div>
         </div>
         <div class="form-group">
           <label for="functionInput">Função:</label>
-          <input type="text" class="form-control" id="functionInput" name="functionInput" placeholder="Sua função">
+          <input type="text" class="form-control" id="functionInput" name="functionInput" placeholder="Sua função" required>
+          <div class="invalid-feedback">Informe uma função válida (somente letras e espaços).</div>
         </div>
         <button type="submit" class="btn btn-success">Registrar</button>
       </form>
     </div>
   </div>
 </div>
+
+<!-- Form Validation JS -->
+<script src="../handles/form_validation.js"></script>
 
 <!-- Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
