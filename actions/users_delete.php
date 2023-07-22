8ae220a28c724e2683e90ca00b0caffccd00b553
root <root@LucasSilva>
2023-07-22 10:56:35 -0300

    Alteração do script para validar a ação de deletar um registro, acionando ela apenas se o usuário confirmar que deseja deletar

--- actions/users_delete.php
+++ actions/users_delete.php
@@ -5,17 +5,21 @@ if(!empty($_GET['id'])) {
 
   $id = $_GET['id'];
 
-  $sqlSelect = "SELECT * FROM usuarios WHERE id=$id";
-
-  $result = $conn->query($sqlSelect);
-
-  if($result->num_rows > 0) {
+  if(isset($_GET['confirm']) && $_GET['confirm'] === 'true') {
     $sqlDelete = "DELETE FROM usuarios WHERE id='$id'";
-    
     $result = $conn->query($sqlDelete);
-
+  } else {
+    // Se o parâmetro "confirmar" não estiver definido ou estiver definido como 'false', redireciona de volta à lista de usuários
+    header("Location: ../views/users.php");
+    exit(); // Encerra a execução do script após o redirecionamento
   }
 }
-
 header("Location: ../views/users.php")
 ?>
+// $id = $_GET['id'];
+// $sqlSelect = "SELECT * FROM usuarios WHERE id=$id";
+// $result = $conn->query($sqlSelect);
+// if($result->num_rows > 0) {
+//   $sqlDelete = "DELETE FROM usuarios WHERE id=$id";
+//   $resultDelete = $conn->query($sqlDelete);
+// } 
